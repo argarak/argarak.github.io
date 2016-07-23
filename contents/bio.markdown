2ef9027a11822e6583cb708c3936d960567a84b5
Argarak <kzer-za@cryptolab.net>
2016-07-23 22:31:32 +0100

    Initial commit for the development wintersmith branch

---
+++ contents/bio.markdown
@@ -0,0 +1,43 @@
+---
+title: "About"
+author: Argarak
+template: about.jade
+---
+
+As you may know, I'm Argarak. I am interested in programming, [music tracking](https://en.wikipedia.org/wiki/Music_tracker), electrical engineering and design. I am self taught in all of those areas and I have been developing software for a year, just about everyday.
+
+I do not know what got me into programming, however I remember that it started with the [Scratch](https://scratch.mit.edu/) (would not recommend the second version due to Adobe Air) graphical language. I made little games or other interactive media. My first was a game based on the Star Control 2 graphics called "Mortal Melee", possibly stolen from that [Precursors](http://www.medievalfuture.com/precursors/main.php) remix with the same name. I used a Shofixti Scout for the ship an you used two attacks to destroy Kohr-Ah buzz-saws that flew towards you. The first was a weak laser that did not have great hit-detection. The second was a Melnorme shot that you could control with your ship's direction. 
+
+After doing Scratch for a while, I decided to do Python afterwards. I grasped it quite quickly however, I only did very uninteresting programs such as some program that reverses some text or some [pygame](http://pygame.org/hifi.html) unfinished experiment. If you do want to learn Python try [Learn Python The Hard Way](http://learnpythonthehardway.org/book/index.html).
+
+After that, I heard of a website called EdX. I made an account and started the CS50x course. At first it was simple, just Scratch. However after that we started learning C and creating programs that could print pyramids or a [greedy algorithm](https://en.wikipedia.org/wiki/Greedy_algorithm). Afterwards, it got even harder. From creating a game of [Breakout!](https://en.wikipedia.org/wiki/Breakout_%28video_game%29) to finishing a web server, I had fun.
+
+I still have that final project to do. I don't know exactly what yet and I probably will have to get to that eventually.
+
+Anyway, after CS50x, I was a bit lost. As you might know from one of my previous blog posts, "Loss of what to do", I explained how I was in a "limbo". I had a skill, but no use for it. That was until I started using Github, started creating some pieces of tat I'll remove eventually, but at least I was doing something. I also made a large achievement contributing to a project on Github by debugging it.
+
+Today, I plan various projects and get them done! I love what I do and I no longer fear the shear complexity and challenge to work on them.
+
+I hope you enjoy reading this blog as much as I do writing it!
+
+------
+
+Software I often use:
+
+* awesome-wm - my favourite windowing manager due to its integrated status bar and simple [Lua](https://www.lua.org/) scripting.
+* ncmpcpp - for playing music, well integrates with my window manager.
+* redshift - so eye-strain is minimal
+* icecat - it's clunky and heavyweight. However, it's the only good free browser when it comes to rendering websites (basically a copy of Iceweasel).
+* emacs - the modular, customise-able, feature-full editor! What I've written this site with.
+* termite with zsh - a simple terminal with a great shell - absolutely necessary to this day.
+* mpv - light and filled with useful modifications - fork of mplayer. Great for online videos.
+
+I use the Dvorak keyboard but I am awful at typing - ironic to say for a programmer. My computer's specs are quite low, but I don't mine crypto-currencies or play badly-written slow 3D games, so it's not very important.
+
+------
+
+Send complaints here: <kzer-za@cryptolab.net>.
+
+If you want to send encrypted mail, use my public key: [https://sks-keyservers.net/pks/lookup?op=get&search=0x8D8D23075F93F925](https://sks-keyservers.net/pks/lookup?op=get&search=0x8D8D23075F93F925).
+
+Note: I usually take a while to answer emails, if you want to get a faster response, contact me on Github.
---
+++ contents/js/angularMain.js
@@ -0,0 +1,156 @@
+hljs.initHighlightingOnLoad();
+
+var app = angular.module("nexus", ["ngMaterial", "ngAnimate"])
+                 .config(function($mdThemingProvider) {
+                     $mdThemingProvider.theme('default')
+                                       .primaryPalette("red")
+                                       .accentPalette("pink")
+                                       .dark();
+                     $mdThemingProvider.theme('light')
+                                       .primaryPalette("red")
+                                       .accentPalette("pink");
+                 });
+
+app.controller("mainController", function($scope, $mdSidenav, $mdDialog) {
+    $scope.openLeftMenu = function() {
+        $mdSidenav("left").toggle();
+    }
+
+    $scope.showAlert = function(ev, title, text, ok) {
+        $mdDialog.show(
+            $mdDialog.alert()
+                     .parent(angular.element(document.querySelector("#popupContainer")))
+                     .clickOutsideToClose(true)
+                     .title(title)
+                     .textContent(text)
+                     .ariaLabel(title)
+                     .ok(ok)
+                     .targetEvent(ev)
+        );
+    }
+});
+
+app.controller("indexGreeting", function($scope) {
+    $scope.getGreeting = function() {
+        var d = new Date();
+        var hrs = d.getHours();
+
+        if(hrs >= 6 && hrs < 12) {
+            return "Good Morning";
+        } else if(hrs >= 12 && hrs < 18) {
+            return "Good Afternoon";
+        } else if(hrs >= 18 && hrs < 21) {
+            return "Good Evening";
+        } else {
+            return "Good Night";
+        }
+    }
+});
+
+app.controller("getMonth", function($scope) {
+    $scope.getMonth = function(index) {
+        var months = ["January", "February", "March",
+                      "April", "May", "June", "July",
+                      "August", "September", "October",
+                      "November", "December"];
+        return months[index];
+    }
+});
+
+app.controller("blogController", function($scope, $timeout, $q, $log, $rootScope, $window) {
+    var self = this;
+    this.isDisabled = false;
+    this.querySearch = querySearch;
+    this.searchTextChange = searchTextChange;
+    this.selectedItemChange = selectedItemChange;
+    this.noCache = true;
+
+    $scope.hideObject = new Object();
+
+    $scope.createHideObject = function(articleTitles) {
+        for(var i = 0; i < articleTitles.length; i++)
+            $scope.hideObject[articleTitles[i]] = false;
+    }
+
+    var updateObject = function(name, query) {
+        if(name.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    function querySearch(query, articleTitles) {
+        var articleObj = new Array();
+        for(var i = 0; i < articleTitles.length; i++) {
+            var tmpobj = new Object();
+            tmpobj.value = articleTitles[i].toLowerCase();
+            tmpobj.display = articleTitles[i];
+            articleObj.push(tmpobj);
+        }
+
+        var results = query ? articleObj.filter(createFilterFor(query)) : articleObj,
+            deferred;
+        if(self.simulateQuery) {
+            deferred = $q.defer();
+            $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+            return deferred.promise;
+        } else {
+            return results;
+        }
+    }
+
+    function searchTextChange(text) {
+        for(var i = 0; i < Object.getOwnPropertyNames($scope.hideObject).length; i++) {
+            var names = Object.getOwnPropertyNames($scope.hideObject);
+            $scope.hideObject[names[i]] = updateObject(names[i], text);
+        }
+    }
+
+    function selectedItemChange(item, urls) {
+        var itemName = item.display
+        var names = Object.getOwnPropertyNames($scope.hideObject);
+        $window.location.href = urls[names.indexOf(itemName)];
+    }
+
+    function createFilterFor(query) {
+        var lowercaseQuery = angular.lowercase(query);
+        return function filterFn(state) {
+            return (state.value.indexOf(lowercaseQuery) === 0);
+        }
+    }
+});
+
+app.controller("microblogController", function($scope) {
+    $scope.get = function() {
+        var url = 'https://pump2rss.com/feed/argarak@pumpyourself.com.atom';
+        feednami.load(url, function(result) {
+            if(result.error) {
+                console.log(result.error);
+            } else {
+                for(var i = 0; i < result.feed.entries.length; i++) {
+                    if(result.feed.entries[i].description != null &&
+                       result.feed.entries[i].description != "<a href='https://pumpyourself.com/argarak'>argarak</a> deleted a note" &&
+                       result.feed.entries[i].description != "<a href='https://pumpyourself.com/argarak'>argarak</a> deleted an image") {
+                     
+                    }
+                }
+            }
+        });
+
+    }
+});
+
+/* app.directive('CustomDirective', function(){
+ *     return {
+ *         restrict:  'E',
+ *         link: function(scope, element, attrs){
+ *             scope.$watch('invoice_html_template', function(){
+ *                 var template = scope.invoice_html_template;
+ *                 element.html(template);
+ *                 $compile(element.contents())(scope);
+ *             });
+ *         },
+ *         template: '<div>{{$scope.invoiceTemplate}}</div>'
+ *     }
+ * });*/
